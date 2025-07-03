338f1d082112cb27ed74ca7e543d102bc5248d04
Mitsuaki Nagao <nagao-mitsuaki@jfe-eng.co.jp>
2025-07-03 15:52:07 +0900

    feat: operation_models.pyの返り値用データクラス準備

---
+++ core/physics/operation_results.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from typing import Dict, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..state import (
+        MassBalanceResults,
+        HeatBalanceResults,
+        WallHeatBalanceResult,
+        LidHeatBalanceResult,
+        MoleFractionResults,
+        VacuumPumpingResult,
+        DownstreamFlowResult,
+    )
+
+
+@dataclass
+class BaseOperationResult:
+    material: "MassBalanceResults"
+    heat: "HeatBalanceResults"
+    heat_wall: Dict[int, "WallHeatBalanceResult"]
+    heat_lid: Dict[str, "LidHeatBalanceResult"]
+
+    def get_record_items(self) -> Dict[str, any]:
+        return {
+            "material": self.material,
+            "heat": self.heat,
+            "heat_wall": self.heat_wall,
+            "heat_lid": self.heat_lid,
+        }
+
+
+@dataclass
+class StopModeResult(BaseOperationResult):
+    pass
+
+
+@dataclass
+class BatchAdsorptionResult(BaseOperationResult):
+    pressure_after_batch_adsorption: float
+
+
+@dataclass
+class FlowAdsorptionResult(BaseOperationResult):
+    total_pressure: float
+
+
+@dataclass
+class EqualizationDepressurizationResult(BaseOperationResult):
+    total_pressure: float
+    diff_press: float
+    downflow_params: "DownstreamFlowResult"
+
+
+@dataclass
+class VacuumDesorptionResult(BaseOperationResult):
+    mol_fraction: Optional["MoleFractionResults"]
+    accum_vacuum_amt: "VacuumPumpingResult"
+    pressure_after_desorption: float
+
+    def get_record_items(self) -> Dict[str, any]:
+        items = super().get_record_items()
+        items["mol_fraction"] = self.mol_fraction
+        items["accum_vacuum_amt"] = self.accum_vacuum_amt
+        return items
+
+
+OperationResult = (
+    StopModeResult
+    | BatchAdsorptionResult
+    | FlowAdsorptionResult
+    | EqualizationDepressurizationResult
+    | VacuumDesorptionResult
+)
